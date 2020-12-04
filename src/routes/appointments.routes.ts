1063ccd5ecd61b79e235ff490222ac78a26ff230
Gabriel Carvalho <braga.gabrielcarvalho@gmail.com>
2020-12-04 13:23:50 -0300

    Appointment creation done
    Creates an appointment and verifys if there's any appointment in the same date and hour

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,6 +1,8 @@
 import { Router } from 'express';
+import { parseISO } from 'date-fns';
 import { getCustomRepository } from 'typeorm';
 import AppointmentsRepository from '../respositories/AppointmentsRepository';
+import CreateAppointmentService from '../services/CreateAppointmentService';
 
 const appointmentsRouter = Router();
 
@@ -9,6 +11,25 @@ appointmentsRouter.get('/', async (request, response) => {
   const appointments = await appointmentsRepository.find();
 
   return response.json(appointments);
+});
+
+appointmentsRouter.post('/', async (request, response) => {
+  try {
+    const { provider, date } = request.body;
+
+    const parsedDate = parseISO(date);
+
+    const createAppointment = new CreateAppointmentService();
+
+    const appointment = await createAppointment.execute({
+      date: parsedDate,
+      provider,
+    });
+
+    return response.json(appointment);
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
 });
 
 export default appointmentsRouter;
